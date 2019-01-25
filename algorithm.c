8d83c78681f3df08c2d336cfd6af33399e5e6885
Damparo <95daniele26@gmail.com>
2019-01-25 10:53:40 +0100

    Added heapSortPriority and related funcionts

---
+++ algorithm.c
@@ -0,0 +1,25 @@
+/*
+** this file is part of a git repository licensed under the
+** 										GNU GPL LICENSE
+** whose integral text is part of the repository itself
+**
+** source for graph related algorithms
+** requires graph.h, list.h and vector.h
+**
+** first created  20/12/2018 (with older materials)
+** version 0      
+** last updated   20/12/2018
+**
+** function count -> 0
+**
+** write to dan(dot)salierno(at)stud(dot)uniroma3(dot)it for comments
+** Daniele Salierno
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "vector.h"
+#include "list.h"
+#include "graph.h"
+#include "algorithm.h"
